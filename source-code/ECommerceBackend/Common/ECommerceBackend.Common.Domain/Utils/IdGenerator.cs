e66a0ab255054a2aa6cb92c87212b81af29d7741
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-08-19 19:50:01 +0700

    feat: add common domain

---
+++ source-code/ECommerceBackend/Common/ECommerceBackend.Common.Domain/Utils/IdGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceBackend.Common.Domain.Utils;
+public static class IdGenerator
+{
+    /// <summary>
+    /// Generates a new unique identifier (ID) using the version 7 format.
+    /// </summary>
+    /// <returns>A new GUID representing the unique identifier.</returns>
+    public static Guid GenerateId() => Guid.CreateVersion7();
+}
