3459a3848ba3a9b8ba7fbe614d1e50e5618e0b2a
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-08-19 17:40:26 +0700

    feat: add swagger

--- source-code/ECommerceBackend/API/EcommerceBackend.Api/Program.cs
+++ source-code/ECommerceBackend/API/EcommerceBackend.Api/Program.cs
@@ -1,11 +1,8 @@
-using System.Security.Cryptography;
-using EcommerceBackend.Api.Models;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// OpenAPI and Swagger configuration
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 WebApplication app = builder.Build();
 
@@ -13,37 +10,15 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
+
+app.MapGet("/", () => "Welcome to the Ecommerce Backend API!");
 
 app.UseHttpsRedirection();
 
-string[] summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
-        new EcommerceBackend.Api.Models.WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            RandomNumberGenerator.GetInt32(-20, 55),
-            summaries[RandomNumberGenerator.GetInt32(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
 await app.RunAsync();
-#pragma warning restore CA2007 // Consider calling ConfigureAwait on the awaited task
-
-namespace EcommerceBackend.Api.Models
-{
-    internal sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-    {
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
-}
