2e3187a4044b342113fae813604e702ccffe7ed2
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-08-20 17:24:22 +0700

    feat: add common infras

---
+++ source-code/ECommerceBackend/Common/ECommerceBackend.Common.Infrastructure/Data/DbConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Common;
+using ECommerceBackend.Common.Application.Data;
+using Npgsql;
+
+namespace ECommerceBackend.Common.Infrastructure.Data;
+internal sealed class DbConnectionFactory : IDbConnectionFactory
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public DbConnectionFactory(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource ?? throw new ArgumentNullException(nameof(dataSource));
+    }
+
+    public async ValueTask<DbConnection> OpenConnectionAsync()
+    {
+        return await _dataSource.OpenConnectionAsync();
+    }
+}
