90ed8a511d39438c04cd7f86c52df48152eb34ef
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-08-20 16:28:32 +0700

    feat: add common application

---
+++ source-code/ECommerceBackend/Common/ECommerceBackend.Common.Application/Messaging/IDomainEventHandler.cs
@@ -0,0 +1,7 @@
+﻿using ECommerceBackend.Common.Domain;
+using MediatR;
+
+namespace ECommerceBackend.Common.Application.Messaging;
+
+public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent>
+    where TDomainEvent : IDomainEvent;
