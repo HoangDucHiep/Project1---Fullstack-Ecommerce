1cadda738b046c9a0c587dd906f613bec7c9f9b5
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-08-20 17:24:22 +0700

    feat: add common infras

---
+++ source-code/ECommerceBackend/Common/ECommerceBackend.Common.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using ECommerceBackend.Common.Application.Clock;
+
+namespace ECommerceBackend.Common.Infrastructure.Clock;
+
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+}
---
+++ source-code/ECommerceBackend/Common/ECommerceBackend.Common.Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using ECommerceBackend.Common.Application.Caching;
+using ECommerceBackend.Common.Application.Clock;
+using ECommerceBackend.Common.Application.Data;
+using ECommerceBackend.Common.Infrastructure.Caching;
+using ECommerceBackend.Common.Infrastructure.Clock;
+using ECommerceBackend.Common.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Npgsql;
+using StackExchange.Redis;
+
+namespace ECommerceBackend.Common.Infrastructure;
+public static class InfrastructureConfiguration
+{
+
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        string databaseConnectionString,
+        string redisConnectionString
+    )
+    {
+        NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnectionString).Build();
+
+        services.TryAddSingleton(npgsqlDataSource);
+        services.TryAddScoped<IDbConnectionFactory, DbConnectionFactory>();
+        services.TryAddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        // redis
+        try
+        {
+            IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
+            services.TryAddSingleton(connectionMultiplexer);
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.ConnectionMultiplexerFactory = () => Task.FromResult(connectionMultiplexer);
+            });
+        }
+        catch
+        {
+            services.AddDistributedMemoryCache();
+        }
+
+        services.TryAddSingleton<ICacheService, CacheService>();
+
+        return services;
+    }
+
+
+}
