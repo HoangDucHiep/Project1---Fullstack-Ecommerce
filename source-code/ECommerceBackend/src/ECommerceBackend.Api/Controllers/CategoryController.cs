94e53e117ea025da4d65b296ad226e8a48b0be30
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-09-24 09:43:36 +0700

    ref: change to Monolith Clean Architecture project for easer works

---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Api/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceBackend.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public sealed class CategoryController : ControllerBase
+{
+
+    [HttpGet]
+    public List<string> GetAll()
+    {
+        var categories = new List<string> { "Electronics", "Clothing", "Books" };
+        return categories;
+    }
+}
