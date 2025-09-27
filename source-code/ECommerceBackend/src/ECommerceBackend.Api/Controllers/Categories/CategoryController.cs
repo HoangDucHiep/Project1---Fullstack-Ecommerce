46c132347cf6eb2303990bb343739e71a0de48a2
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-09-27 22:05:51 +0700

    add init for cate

---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Api/Controllers/Categories/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceBackend.Api.Controllers.Categories;
+
+
+[ApiController]
+[Route("api/v1/categories")]
+public class CategoryController : ControllerBase
+{
+    // Implement category-related endpoints here
+    private readonly ISender _sender;
+
+    public CategoryController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+
+}
