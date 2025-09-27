e5af91dfe0123891ee1f3b1419eddf0e124c1bd8
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-09-27 21:48:04 +0700

    feat: add shop regis endpoint

---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Api/Controllers/Shops/ShopRegiesterRequest.cs
@@ -0,0 +1,9 @@
+﻿using ECommerceBackend.Domain.Shops;
+
+namespace ECommerceBackend.Api.Controllers.Shops;
+
+public sealed record ShopRegiesterRequest(
+    string Name, string Description,
+    string LogoUrl, string BannerUrl,
+    ShopStatus Status, Guid OwnerId
+);
---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Application/Shops/ShopRegister/ShopRegisterCommand.cs
@@ -0,0 +1,8 @@
+﻿using ECommerceBackend.Application.Abstracts.Messaging;
+
+namespace ECommerceBackend.Application.Shops.ShopRegister;
+public sealed record ShopRegisterCommand(
+    string Name, string Description,
+    string LogoUrl, string BannerUrl,
+    Guid OwnerId
+) : ICommand<Guid>;
