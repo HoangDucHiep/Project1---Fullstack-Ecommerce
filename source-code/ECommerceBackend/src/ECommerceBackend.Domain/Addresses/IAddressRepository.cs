ebe7d5227e4c1c177c3dd9fe4c76c09e794c4941
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-09-30 19:18:32 +0700

    feat: add adress domain

---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Domain/Addresses/IAddressRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceBackend.Domain.Addresses;
+public interface IAddressRepository
+{
+    Task<List<Address>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Address?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    void Add(Address address);
+
+    Task<List<Address>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    int Update(Address address);
+}
---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Infrastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceBackend.Domain.Addresses;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceBackend.Infrastructure.Repositories;
+public class AddressRepository : Repository<Address>, IAddressRepository
+{
+    public AddressRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<List<Address>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        return (List<Address>?)await _dbContext.Set<Address>()
+            .Where(a => a.UserId == userId)
+            .ToListAsync(cancellationToken);
+    }
+
+    public int Update(Address address)
+    {
+        return _dbContext.Set<Address>().Update(address).State == EntityState.Modified ? 1 : 0;
+    }
+}
