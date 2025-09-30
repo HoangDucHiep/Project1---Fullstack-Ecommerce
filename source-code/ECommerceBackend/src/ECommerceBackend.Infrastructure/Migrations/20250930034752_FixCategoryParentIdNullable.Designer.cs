b305c27f6db022326c1f4838cc2ce63c41aa4de3
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-09-30 10:49:32 +0700

    fix: change parent_id of category to nullable

--- source-code/ECommerceBackend/src/ECommerceBackend.Infrastructure/Migrations/20250930034752_FixCategoryParentIdNullable.Designer.cs
+++ source-code/ECommerceBackend/src/ECommerceBackend.Infrastructure/Migrations/20250930034752_FixCategoryParentIdNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ECommerceBackend.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ECommerceBackend.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250930034752_FixCategoryParentIdNullable")]
+    partial class FixCategoryParentIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
