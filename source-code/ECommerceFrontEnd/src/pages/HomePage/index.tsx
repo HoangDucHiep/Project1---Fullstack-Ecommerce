ccbc5a8a3c9bb456b4fac16a517260712068a319
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2025-10-01 14:11:14 +0700

    Header+Footer layout demo

--- source-code/ECommerceFrontEnd/src/pages/HomePage/index.tsx
+++ source-code/ECommerceFrontEnd/src/pages/HomePage/index.tsx
@@ -1,8 +1,15 @@
+import Footer from "../../layouts/Footer";
+import Header from "../../layouts/Header";
+import CategoryManergerComponent from "../../components/CategoryManergerComponent/CategoryManergerComponent.tsx";
 
 const HomePage= () => {
     return (
         <>
+          <Header />
           <h1>Trang chủ</h1>
+            <CategoryManergerComponent />
+          <Footer/>
+
         </>
     )
 }
