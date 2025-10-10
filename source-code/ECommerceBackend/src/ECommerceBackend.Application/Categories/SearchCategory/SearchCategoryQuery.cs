ff1d99acf1f6dbdde0d0f4ea60c1bda0df03266c
Ca11MeHCL <minhphung755@gmail.com>
2025-10-10 16:33:50 +0700

    Done

---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Application/Categories/SearchCategory/SearchCategoryQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceBackend.Application.Abstracts.Messaging;
+using ECommerceBackend.Application.Contracts.Categories;
+using ECommerceBackend.Application.Users.GetUserById;
+
+namespace ECommerceBackend.Application.Categories.SearchCategory;
+
+/// PBNMinh- 08/09/2025
+public sealed record SearchCategoryQuery(string QueryText) : IQuery<List<CategoryDto>>;
+
