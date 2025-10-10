0d120880b68bc03fc8db6b1ef8bd2a3926f3bf55
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-10-10 15:56:25 +0700

    temp: quick add login endpoint

---
+++ source-code/ECommerceBackend/src/ECommerceBackend.Application/Authentication/LoginUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceBackend.Application.Abstracts.Authentication;
+using ECommerceBackend.Application.Abstracts.Messaging;
+using ECommerceBackend.Domain.Abstracts;
+
+namespace ECommerceBackend.Application.Authentication;
+
+/// HDHiep - 10/10/2025
+/// <summary>
+/// Handler for login user command
+/// </summary>
+public sealed class LoginUserCommandHandler : ICommandHandler<LoginUserCommand, AuthenticationResult>
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public LoginUserCommandHandler(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    public async Task<Result<AuthenticationResult>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        return await _authenticationService.LoginAsync(request.Identifier, request.Password);
+    }
+}
