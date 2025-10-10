a97b592df386aa97f1a3229581e4a5c54e9a1760
Ca11MeHCL <minhphung755@gmail.com>
2025-10-10 23:03:16 +0700

    DeleteAddress

--- source-code/ECommerceBackend/src/ECommerceBackend.Application/Addresses/DeleteAddress/DeleteAddressCommand.cs
+++ source-code/ECommerceBackend/src/ECommerceBackend.Application/Addresses/DeleteAddress/DeleteAddressCommand.cs
@@ -1,6 +1,4 @@
 ﻿using ECommerceBackend.Application.Abstracts.Messaging;
 
 namespace ECommerceBackend.Application.Addresses.DeleteAddress;
-public sealed record DeleteAddressCommand(
-    Guid AddressId
-    ) : ICommand;
+public sealed record DeleteAddressCommand(Guid AddressId, Guid UserId) : ICommand;
