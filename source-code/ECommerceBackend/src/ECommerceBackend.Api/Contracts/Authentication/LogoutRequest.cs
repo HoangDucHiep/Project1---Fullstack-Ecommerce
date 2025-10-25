1a0fd68d1a56d272bf01109e167d1802557516eb
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-10-26 00:56:35 +0700

    fix: better logout to use authed user instead of manually passing user id, better get me

--- source-code/ECommerceBackend/src/ECommerceBackend.Api/Contracts/Authentication/LogoutRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace ECommerceBackend.Api.Contracts.Authentication;
-
-/// <summary>
-/// Request model for user logout
-/// </summary>
-/// <param name="UserId">The user ID to logout</param>
-public record LogoutRequest(
-    string UserId
-);
