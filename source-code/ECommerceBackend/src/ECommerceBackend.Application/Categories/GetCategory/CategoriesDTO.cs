a952a7c596602c1268a0f07ac9e7a94dbf1a3192
Ca11MeHCL <minhphung755@gmail.com>
2025-10-26 19:02:21 +0700

    refactor: search to get category tree

--- source-code/ECommerceBackend/src/ECommerceBackend.Application/Categories/GetCategory/CategoriesDTO.cs
+++ source-code/ECommerceBackend/src/ECommerceBackend.Application/Categories/GetCategory/CategoriesDTO.cs
@@ -11,8 +11,6 @@ public sealed class CategoriesDTO
     public string IconUrl { get; set; } // Đổi từ private set -> public set
     public CategoryStatus Status { get; set; } // Đổi từ private set -> public set
     public Guid? ParentId { get; set; } // Đổi từ private set -> public set
-    public int Lft { get; set; } // Đổi từ private set -> public set
-    public int Rgt { get; set; } // Đổi từ private set -> public set
     public int Depth { get; set; } // Đổi từ private set -> public set
 
     public DateTimeOffset CreatedAtUtc { get; set; } // Đổi từ private set -> public set
