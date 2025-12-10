613bef1d021abcbb217c6131b651acc963d68b62
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-12-11 01:29:41 +0700

    init auth-service

---
+++ source_code/apps/api-gateway/src/main.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
+import axios from "axios";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Middleware
+// Enable CORS
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));                                                 // Logging
+app.use(express.json({ limit: "100mb" }));                              // Body Parsing
+app.use(express.urlencoded({ limit: "100mb", extended: true }));        // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());                                                // Cookie Parsing
+app.set("trust proxy", 1);                                              // Trust first proxy
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: "Too many requests, please try again later." },
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: (req: any) => req.ip
+});
+
+app.use(limiter);
+
+app.get("/gateway-health", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
+});
+
+// proxies
+app.use("/", proxy("http://localhost:6001")); // auth-service
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+server.on("error", console.error);
